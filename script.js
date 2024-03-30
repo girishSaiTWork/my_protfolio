834b775681430bcdce45a40e9c23905cfefb0d11
girishSaiTWork <thiruvidhula2022@gmail.com>
2024-03-29 21:24:43 -0400

    ResumeLinkUpdate

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ function toggleMenu() {
 }
 
 function openGoogleDrive() {
-  window.open('https://drive.google.com/file/d/1DY9AxVmX7doJAXrcPqqL7Y97U124w9tf/view?usp=drive_link');
+  window.open('https://drive.google.com/file/d/1fMD9YDZIEHV3BO4aAvtKn__dUoc6et9E/view?usp=sharing');
 }
